bcf82bcd93d41c11c470a37a15717d4ad8e1f1a9
StepanChuev <StepanChuev>
2024-09-16 19:11:03 +0400

    Safety reallocating tokens and normalized

--- src/lexer.c
+++ src/lexer.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <limits.h>
 #include "lexer.h"
 
 Token *getTokensFromExpression(char *expression){
-	int lenTokens = 10000;
+	int lenTokens = 1000;
 	int tokensIndex = 0;
 	int startValueIndex = 0;
 	int lenTokenValue = 0;
@@ -16,6 +17,18 @@ Token *getTokensFromExpression(char *expression){
 	strcpy(tokens[0].name, getTokenName(expression, 0));
 
 	for (int i = 1; expression[i] != '\0'; i++){
+		if (tokensIndex + 1 >= lenTokens){
+			if ((long long int)lenTokens * 2 >= INT_MAX){
+				lenTokens = INT_MAX;
+			}
+
+			else {
+				lenTokens *= 2;
+			}
+
+			tokens = (Token *)realloc(tokens, lenTokens * sizeof(Token));
+		}
+
 		if (strcmp(tokens[tokensIndex].name, getTokenName(expression, i))){
 			lenTokenValue = i - startValueIndex;
 
@@ -50,13 +63,26 @@ const char *getTokenName(char *expression, int index){
 }
 
 char *normalize(char *srcExp){
-	int lenNormalized = 10000;
+	const int limit = INT_MAX - 2;
+	int lenNormalized = 1000;
 	int normalizedIndex = 0;
 	char *normalized = (char *)malloc(lenNormalized * sizeof(char));
 
 	for (int i = 0; srcExp[i] != '\0'; i++){
 		if (srcExp[i] == ' '){
 			continue;
+		}
+
+		if (normalizedIndex + 1 >= lenNormalized){
+			if ((long long int)lenNormalized * 2 >= limit){
+				lenNormalized = limit;
+			}
+
+			else {
+				lenNormalized *= 2;
+			}
+
+			normalized = (char *)realloc(normalized, lenNormalized * sizeof(char));
 		}
 
 		normalized[normalizedIndex++] = srcExp[i];
