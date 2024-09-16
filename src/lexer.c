72ff6b5a851fbe73f51e92c3e57889cb1b614a01
StepanChuev <StepanChuev>
2024-09-16 17:28:17 +0400

    Small refactoring

--- src/lexer.c
+++ src/lexer.c
@@ -5,37 +5,29 @@
 
 Token *getTokensFromExpression(char *expression){
 	int lenTokens = 10000;
-	int maxLenTokenValue = 100;
 	int tokensIndex = 0;
 	int startValueIndex = 0;
-	int amountSpaces = 0;
 	int lenTokenValue = 0;
 
 	Token *tokens = (Token *)malloc(lenTokens * sizeof(Token));
 	tokens[0].name = (char *)malloc(MAX_LEN_TOKEN_NAME * sizeof(char));
-	tokens[0].value = (char *)malloc(maxLenTokenValue * sizeof(char));
+	tokens[0].value = (char *)malloc(MAX_LEN_TOKEN_VALUE * sizeof(char));
 
 	strcpy(tokens[0].name, getTokenName(expression, 0));
 
 	for (int i = 1; expression[i] != '\0'; i++){
-		if (expression[i] == ' '){
-			amountSpaces++;
-			continue;
-		}
-
 		if (strcmp(tokens[tokensIndex].name, getTokenName(expression, i))){
-			lenTokenValue = i - startValueIndex - amountSpaces;
+			lenTokenValue = i - startValueIndex;
 
 			strncpy(tokens[tokensIndex].value, expression + startValueIndex, lenTokenValue);
 			tokens[tokensIndex].value[lenTokenValue] = '\0';
 
 			tokens[++tokensIndex].name = (char *)malloc(MAX_LEN_TOKEN_NAME * sizeof(char));
-			tokens[tokensIndex].value = (char *)malloc(maxLenTokenValue * sizeof(char));
+			tokens[tokensIndex].value = (char *)malloc(MAX_LEN_TOKEN_VALUE * sizeof(char));
 
 			strcpy(tokens[tokensIndex].name, getTokenName(expression, i));
 
 			startValueIndex = i;
-			amountSpaces = 0;
 		}
 	}
 
@@ -57,17 +49,17 @@ const char *getTokenName(char *expression, int index){
 	return END_TOKEN;
 }
 
-char *normalize(char *expression){
+char *normalize(char *srcExp){
 	int lenNormalized = 10000;
 	int normalizedIndex = 0;
 	char *normalized = (char *)malloc(lenNormalized * sizeof(char));
 
-	for (int i = 0; expression[i] != '\0'; i++){
-		if (expression[i] == ' '){
+	for (int i = 0; srcExp[i] != '\0'; i++){
+		if (srcExp[i] == ' '){
 			continue;
 		}
 
-		normalized[normalizedIndex++] = expression[i];
+		normalized[normalizedIndex++] = srcExp[i];
 	}
 
 	strcpy(normalized + normalizedIndex, END_TOKEN);
--- src/lexer.h
+++ src/lexer.h
@@ -13,6 +13,7 @@
 #define ALL_OPERATORS "+-*/"
 
 #define MAX_LEN_TOKEN_NAME 4
+#define MAX_LEN_TOKEN_VALUE 312
 
 typedef struct {
 	char *name;
@@ -21,6 +22,6 @@ typedef struct {
 
 Token *getTokensFromExpression(char *expression);
 const char *getTokenName(char *expression, int index);
-char *normalize(char *expression);
+char *normalize(char *srcExp);
 
 #endif
